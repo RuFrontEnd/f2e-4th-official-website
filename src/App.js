a15fe2deb1913202fe09fe74612de7b6f87f8fec
RuFrontEnd <k718198@gmail.com>
2022-10-09 16:35:17 +0800

    切版 navbar 電腦版

--- src/App.js
+++ src/App.js
@@ -6,17 +6,28 @@ import hexLogo from "assets/hex-logo.png";
 
 function App() {
   return (
-    <header className="header main-color">
-      <Container>
-        <Row>
-          <Col>
-            <img src={hexLogo} alt="hexLogo"></img>
-          </Col>
-          <Col>123</Col>
-          <Col>123</Col>
-        </Row>
-      </Container>
-    </header>
+    <>
+      <header className="header">
+        <Container>
+          <Row>
+            <Col>
+              <div className="header-wrapper d-flex justify-content-between align-items-center">
+                <img src={hexLogo} alt="hex-logo" width="145" height="35"></img>
+                <ul className="header-items d-flex justify-content-end align-items-center">
+                  <li className="header-item ms-4">關卡任務</li>
+                  <li className="header-item ms-4">競賽說明</li>
+                  <li className="header-item ms-4">
+                    <button className="sign-up-btn pt-2 pb-2 ps-4 pe-4 rounded-pill">
+                      立即報名
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+    </>
   );
 }
 
