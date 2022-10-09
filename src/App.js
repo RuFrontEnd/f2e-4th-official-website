fb8b2fb37bacbdc64747b7e64a102d34e7a3789c
RuFrontEnd <k718198@gmail.com>
2022-10-09 21:13:30 +0800

    新增 v 怪客背景圖

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Header from "section/header/header";
+import Header from "section/header/Header";
+import Main from "section/main/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
