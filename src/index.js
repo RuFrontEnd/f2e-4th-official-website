cb9e594642426e2995d6ef017a32814e69487be9
RuFrontEnd <k718198@gmail.com>
2022-10-16 15:35:11 +0800

    修正 rwd 斷點

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./bootstrap.custom.scss";
+import "./bootstrap.theme.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
