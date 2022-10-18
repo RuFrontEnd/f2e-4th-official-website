29c30dfd48a2dcdac39cad3fec69560489f00dbc
RuFrontEnd <k718198@gmail.com>
2022-10-18 21:20:39 +0800

    完成 Topic 區塊 TopicCard

--- src/components/topicCard/TopicCard.js
+++ src/components/topicCard/TopicCard.js
@@ -1,38 +1,38 @@
 import styles from "./topicCard.module.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import blockStudio from "assets/block-studio.svg";
-import parallex from "assets/parallex.svg";
-import pdf from "assets/pdf.svg";
 import Button from "components/button/Button";
 
 function TopicCard(props) {
   return (
     <div className={`${styles["topic-card"]} ${props.className}`}>
-      <div className={"d-flex justify-content-between"}>
-        <p className={`${styles["title"]}`}>視差滾動 Parallax Scrolling</p>
-        <img src={blockStudio} width={327} height={69}></img>
+      <div
+        className={`${styles["top-content"]} d-flex justify-content-between`}
+      >
+        <p className={`${styles["title"]}`}>{props.title}</p>
+        <img
+          className={`${styles["company-img"]}`}
+          src={props.company?.src}
+          width={props.company?.width}
+          height={70}
+        ></img>
       </div>
       <div className={`${styles.content} d-flex`}>
         <div
           className={`${styles["img-wrapper"]} h-100 d-flex align-items-center`}
         >
-          <img src={pdf} width={320} height={290}></img>
+          <img src={props.img} width={320} height={290}></img>
         </div>
         <div className={`d-flex flex-column justify-content-between`}>
           <div>
-            <p className={`${styles.project}`}>The F2E 活動網站設計</p>
-            <p className={`${styles.illustrate}`}>
-              請參考本屆官網的首頁視差滾動技巧，並請您重新 redesign
-              本頁面來設計。
-            </p>
+            <p className={`${styles.project}`}>{props.project}</p>
+            <p className={`${styles.illustrate}`}>{props.illustrate}</p>
           </div>
           <div>
-            <Button className={`${styles.raiders}`} secondary>關卡攻略</Button>
+            <Button className={`${styles.raiders}`} secondary>
+              關卡攻略
+            </Button>
             <Button className={`${styles.post}`}>投稿</Button>
           </div>
-        </div>s
+        </div>
       </div>
     </div>
   );
--- src/section/topic/Topic.js
+++ src/section/topic/Topic.js
@@ -7,6 +7,12 @@ import bounty from "assets/bounty.svg";
 import mouseMiddleBtn from "assets/mouse-middle-button.svg";
 import mouseBody from "assets/mouse-body.svg";
 import TopicCard from "components/topicCard/TopicCard";
+import blockStudio from "assets/block-studio.svg";
+import kdan from "assets/kdan.svg";
+import titan from "assets/titan.svg";
+import parallex from "assets/parallex.svg";
+import pdf from "assets/pdf.svg";
+import scrum from "assets/scrum.svg";
 
 function Topic() {
   return (
@@ -18,7 +24,12 @@ function Topic() {
               <div
                 className={`d-flex flex-column justify-content-center align-items-center`}
               >
-                <img src={hexScholl} width={590} height={146} className={styles['hex-scholl']}></img>
+                <img
+                  src={hexScholl}
+                  width={590}
+                  height={146}
+                  className={styles["hex-scholl"]}
+                ></img>
                 <div
                   className={`${styles.copyWriting} d-flex justify-content-center`}
                 >
@@ -31,13 +42,40 @@ function Topic() {
               </div>
             </Col>
             <Col sm={12} md={6} lg={12}>
-              <TopicCard className={styles["topic-card"]} />
+              <TopicCard
+                className={styles["topic-card"]}
+                title="視差滾動  Parallax Scrolling"
+                company={{ src: blockStudio, width: 327 }}
+                img={parallex}
+                project={"The F2E 活動網站設計"}
+                illustrate={
+                  "請參考本屆官網的首頁視差滾動技巧，並請您重新 redesign 本頁面來設計。"
+                }
+              />
             </Col>
             <Col sm={12} md={6} lg={12}>
-              <TopicCard className={styles["topic-card"]} />
+              <TopicCard
+                className={styles["topic-card"]}
+                title="線上簽署  Canvas"
+                company={{ src: kdan, width: 160 }}
+                img={pdf}
+                project={"今晚，我想來點點簽"}
+                illustrate={
+                  "每次要 PDF 簽名都要列印出來再掃描好麻煩，自幹一個 Web 版本的簽名服務吧！"
+                }
+              />
             </Col>
             <Col sm={12} md={6} lg={12}>
-              <TopicCard className={styles["topic-card"]} />
+              <TopicCard
+                className={styles["topic-card"]}
+                title="Scrum  JS draggable"
+                company={{ src: titan, width: 299 }}
+                img={scrum}
+                project={"Scrum 新手村"}
+                illustrate={
+                  "設計關卡內容與網頁互動效果，透過頁面關卡來呈現知識點，讓挑戰者藉由你設計的內容更了解 Scrum。"
+                }
+              />
             </Col>
           </Row>
         </Container>
